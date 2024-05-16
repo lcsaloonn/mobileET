9ca81d0b931be93801e11f290aed42ffae45f455
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2024-05-16 21:24:09 +0200

    added mapper

--- app/(tabs)/screens/create.tsx
+++ app/(tabs)/screens/create.tsx
@@ -1,14 +1,29 @@
 import { View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import AlbumHeaderButtons from "components/albumHeaderButtons/albumHeaderButtons.component";
 
 const create = () => {
+  const categories = ["gallery", "moments", "map"] as const;
+  type Categories = (typeof categories)[number];
+  const [selectedType, setSelectedTyped] = useState<string>(categories[0]);
+
+  const map: { [key in Categories]: JSX.Element } = {
+    gallery: <Text>Gallery</Text>,
+    moments: <Text>Not Available for the moment</Text>,
+    map: <Text>Not Available for the moment</Text>,
+  };
+
   return (
     // album view in here for the moment
     <SafeAreaView className="h-full mx-4 my-4">
       <View>
-        <AlbumHeaderButtons />
+        <AlbumHeaderButtons
+          selectedType={(name) => setSelectedTyped(name)}
+          catgories={categories as [string, string, string]}
+          defaultIndexSelection={0}
+        />
+        {map[selectedType]}
       </View>
       {/* square for the different views by 2 or more */}
     </SafeAreaView>
--- components/albumHeaderButtons/albumHeaderButtons.component.tsx
+++ components/albumHeaderButtons/albumHeaderButtons.component.tsx
@@ -1,23 +1,34 @@
 import { useState } from "react";
 import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 
-const catgories = ["Gallery", "Moments", "Map"];
+type TAlbumHeaderButtons = {
+  selectedType: (name: string) => void;
+  catgories: [string, string, string];
+  defaultIndexSelection: 0 | 1 | 2;
+};
 
-const AlbumHeaderButtons = () => {
-  const [selectedButton, setSelectedButton] = useState(0);
-  const btnPress = (index: number) => setSelectedButton(index);
+const AlbumHeaderButtons = ({
+  selectedType,
+  catgories,
+  defaultIndexSelection,
+}: TAlbumHeaderButtons) => {
+  const [selectedButton, setSelectedButton] = useState(defaultIndexSelection);
+  const btnPress = (index: 0 | 1 | 2, categorie: string) => {
+    setSelectedButton(index);
+    selectedType(categorie);
+  };
   return (
     <View className="justify-between flex-row">
-      {catgories.map((element, index) => (
+      {catgories.map((element, index: 0 | 1 | 2) => (
         <TouchableOpacity
           className={`${
             selectedButton == index ? "bg-primary" : " bg-gray-300"
           }`}
           style={styles.button}
           key={index}
-          onPress={() => btnPress(index)}
+          onPress={() => btnPress(index, element)}
         >
-          <Text className="text-white text-center">{element}</Text>
+          <Text className="text-white text-center capitalize">{element}</Text>
         </TouchableOpacity>
       ))}
     </View>
