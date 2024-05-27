450faf0b276b5fe42de7f761d7ff07a5559fa5c3
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2024-05-27 20:46:13 +0200

    remove error

--- app/(tabs)/screens/create.tsx
+++ app/(tabs)/screens/create.tsx
@@ -3,46 +3,11 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import React, { useEffect, useState } from "react";
 import AlbumHeaderButtons from "components/molecules/albumHeaderButtons/albumHeaderButtons.component";
 import ImageComponent from "components/atoms/image/image.component";
-import { albumsMock } from "mocks/albums.mock";
 
 const create = () => {
   const categories = ["gallery", "moments", "map"] as const;
   type Categories = (typeof categories)[number];
   const [selectedType, setSelectedTyped] = useState<string>(categories[0]);
-
-  const map: { [key in Categories]: JSX.Element } = {
-    gallery: (
-      <FlatList
-        data={albumsMock}
-        keyExtractor={(item) => item.id}
-        numColumns={3}
-        renderItem={({ index, item }) => (
-          <ImageComponent picture={item.mainPicture} className="mx-auto my-1" />
-        )}
-      />
-    ),
-
-    moments: (
-      <Text
-        className="text-3xl font-pblack text-center  mt-28"
-        style={{
-          textAlignVertical: "center",
-        }}
-      >
-        Not Available for the moment
-      </Text>
-    ),
-    map: (
-      <Text
-        className="text-3xl font-pblack text-center mt-28"
-        style={{
-          textAlignVertical: "center",
-        }}
-      >
-        Not Available for the moment
-      </Text>
-    ),
-  };
 
   return (
     // album view in here for the moment
@@ -53,7 +18,6 @@ const create = () => {
           catgories={categories as [string, string, string]}
           defaultIndexSelection={0}
         />
-        <View className="h-full mt-7">{map[selectedType]}</View>
       </View>
       {/* square for the different views by 2 or more */}
     </SafeAreaView>
