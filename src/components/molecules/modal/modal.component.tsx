dca8b4767a3b128c836f11a51ac89c2152398532
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2024-06-11 18:49:14 +0200

    modal creation

---
+++ src/components/molecules/modal/modal.component.tsx
@@ -0,0 +1,39 @@
+import {
+  Modal as RNModal,
+  ModalProps,
+  KeyboardAvoidingView,
+  Platform,
+  View,
+} from "react-native";
+
+type TModal = ModalProps & {
+  isOpen: boolean;
+  withInput?: boolean;
+};
+const Modal = ({ isOpen, withInput, children, ...rest }: TModal) => {
+  const content = withInput ? (
+    <KeyboardAvoidingView
+      className="item-center justify-center flex-1 px-3 bg-zinc-900/40"
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+    >
+      {children}
+    </KeyboardAvoidingView>
+  ) : (
+    <View className="items-center justify-center flex-1 px-3  bg-zinc-900/40">
+      {children}
+    </View>
+  );
+  return (
+    <RNModal
+      visible={isOpen}
+      transparent
+      animationType="fade"
+      statusBarTranslucent
+      {...rest}
+    >
+      {content}
+    </RNModal>
+  );
+};
+
+export default Modal;
